5514a22a6764a3edbe4d746a724794a8f8695994
seguncode <vagbenro@gmail.com>
2022-06-22 04:39:50 +0100

    update body

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,8 +2,8 @@ import Header from './header'
 
 function Body({ children }){
     return(
-        <div className="w-screen h-screen overflow-x-hidden bg-[#F3F3F3]">
-            <div className="w-full  2lg:w-on-lg h-full mx-auto">
+        <div className="w-screen h-screen overflow-x-hidden bg-grey-200">
+            <div className="w-full px-6 2lg:w-on-lg h-full mx-auto">
                 <Header />
                 { children }
             </div>
